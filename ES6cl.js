45638c19fb99834416707ba124576f1b62589cd7
cruz <cruuzzjr@gmail.com>
2022-04-19 14:06:20 -0600

    Video notes start

---
+++ ES6cl.js
@@ -0,0 +1,95 @@
+
+//? Const and let
+
+//! Var:
+    //* This keyword is used to declare variable globally. If you used this keyword to declare 
+    //*   variable then the variable can accessible globally and changeable also.
+    //*     It is good for a short length of codes, if the codes get huge then you will get confused. 
+//! For:
+    //* This keyword is used to declare variable locally. 
+    //*   If you used this keyword to declare variable then the variable can accessible locally and it is changeable as well. 
+    //*     It is good if the code gets huge. 
+//! Const:
+    //* This keyword is used to declare variable locally. 
+    //*   If you use this keyword to declare a variable then the variable will only be accessible within 
+    //*     that block similar to the variable defined by using let and difference between let and const is 
+    //*       that the variables declared using const values can’t be reassigned. So we should assign the value 
+    //*         while declaring the variable.
+
+var x=19;
+{
+    let x=2;
+}
+
+const pi = 3.14159
+
+console.log (pi)
+
+//!  pi=6;
+
+//!  console.log (pi)
+
+//? Template Literals:
+//*  Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, 
+//*    for string interpolation with embedded expressions, and for special constructs called tagged templates.
+
+///*  Template literals are sometimes informally called template strings, because they are used most commonly for string interpolation
+//*    (to create strings by doing substitution of placeholders). However, a tagged template literal may not result in a string;
+//*      it can be used with a custom tag function to perform whatever operations you want on the different parts of the template literal.
+
+let mystring = `Hello
+world
+my name
+is cruz.`;
+
+console.log ( mystring );
+
+let anoutherstring ='hello\nworld \nmy name \nis cruz.'; //* This string is the same as the other but its more dificult to read
+
+console.log (anoutherstring);
+
+console.log(`six times five = ${5 * 6}`);
+
+//? Arrow functions
+
+//* An arrow function expression is a compact alternative to a traditional function expression,
+//*   but is limited and can't be used in all situations.
+
+//* There are differences between arrow functions and traditional functions,
+//*   as well as some limitations:
+
+//* Arrow functions don't have their own bindings to this, arguments or super,
+//*  and should not be used as methods.
+
+//* Arrow functions don't have access to the new.target keyword.
+
+//* Arrow functions aren't suitable for call, apply and bind methods, 
+//*   which generally rely on establishing a scope.
+
+//* Arrow functions cannot be used as constructors.
+
+//* Arrow functions cannot use yield, within its body.
+
+
+let createFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+
+let somefunction = (a,b) => {
+    let result = '';
+    for (let i = 0; i < b; i++) {
+       
+        result += a;
+        
+    }
+    return result
+};
+
+
+console.log(createFullName('Cruz','Garcia'));
+
+console.log(somefunction('Hello' , 3));
+
+//? CallBacks
+
+//* A callback function is a function passed into another function as an argument, 
+//*   which is then invoked inside the outer function to complete some kind of routine or action.
